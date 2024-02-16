cc3fb9c45ad004d4fea0f92d2b98925551d6b198
Dean Heffernan <dean.heffernan@nextpracticehealth.com>
2024-02-16 15:20:16 +1100

    final working version

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,39 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import {Metadata} from "next";
+import {fetchCustomerPages, fetchInvoicesPages} from "@/app/lib/data";
+import {lusitana} from "@/app/ui/fonts";
+import Search from "@/app/ui/search";
+import {Suspense} from "react";
+import {InvoicesTableSkeleton} from "@/app/ui/skeletons";
+import Pagination from "@/app/ui/invoices/pagination";
+import CustomersTable from "@/app/ui/customers/table";
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+
+export default async function Page({
+                                     searchParams,
+                                   }: {
+  searchParams?: {
+    query?: string; page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchCustomerPages(query);
+
+  return (<div className="w-full">
+    <div className="flex w-full items-center justify-between">
+      <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
+        Customers
+      </h1>
+    </div>
+    <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton/>}>
+      <CustomersTable query={query} currentPage={currentPage}/>
+    </Suspense>
+    <div className="mt-5 flex w-full justify-center">
+      <Pagination totalPages={totalPages}/>
+    </div>
+  </div>)
 }
--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -1,22 +1,19 @@
 import Image from 'next/image';
-import { lusitana } from '@/app/ui/fonts';
+import {lusitana} from '@/app/ui/fonts';
 import Search from '@/app/ui/search';
-import {
-  CustomersTableType,
-  FormattedCustomersTable,
-} from '@/app/lib/definitions';
+import {fetchFilteredCustomers} from "@/app/lib/data";
 
 export default async function CustomersTable({
-  customers,
-}: {
-  customers: FormattedCustomersTable[];
+                                               query,
+                                               currentPage,
+                                             }: {
+  query: string;
+  currentPage: number;
 }) {
+  const customers = await fetchFilteredCustomers(query, currentPage)
   return (
     <div className="w-full">
-      <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
-        Customers
-      </h1>
-      <Search placeholder="Search customers..." />
+      <Search placeholder="Search customers..."/>
       <div className="mt-6 flow-root">
         <div className="overflow-x-auto">
           <div className="inline-block min-w-full align-middle">
@@ -27,7 +24,8 @@ export default async function CustomersTable({
                     key={customer.id}
                     className="mb-2 w-full rounded-md bg-white p-4"
                   >
-                    <div className="flex items-center justify-between border-b pb-4">
+                    <div
+                      className="flex items-center justify-between border-b pb-4">
                       <div>
                         <div className="mb-2 flex items-center">
                           <div className="flex items-center gap-3">
@@ -46,7 +44,8 @@ export default async function CustomersTable({
                         </p>
                       </div>
                     </div>
-                    <div className="flex w-full items-center justify-between border-b py-5">
+                    <div
+                      className="flex w-full items-center justify-between border-b py-5">
                       <div className="flex w-1/2 flex-col">
                         <p className="text-xs">Pending</p>
                         <p className="font-medium">{customer.total_pending}</p>
@@ -62,56 +61,63 @@ export default async function CustomersTable({
                   </div>
                 ))}
               </div>
-              <table className="hidden min-w-full rounded-md text-gray-900 md:table">
-                <thead className="rounded-md bg-gray-50 text-left text-sm font-normal">
-                  <tr>
-                    <th scope="col" className="px-4 py-5 font-medium sm:pl-6">
-                      Name
-                    </th>
-                    <th scope="col" className="px-3 py-5 font-medium">
-                      Email
-                    </th>
-                    <th scope="col" className="px-3 py-5 font-medium">
-                      Total Invoices
-                    </th>
-                    <th scope="col" className="px-3 py-5 font-medium">
-                      Total Pending
-                    </th>
-                    <th scope="col" className="px-4 py-5 font-medium">
-                      Total Paid
-                    </th>
-                  </tr>
+              <table
+                className="hidden min-w-full rounded-md text-gray-900 md:table">
+                <thead
+                  className="rounded-md bg-gray-50 text-left text-sm font-normal">
+                <tr>
+                  <th scope="col" className="px-4 py-5 font-medium sm:pl-6">
+                    Name
+                  </th>
+                  <th scope="col" className="px-3 py-5 font-medium">
+                    Email
+                  </th>
+                  <th scope="col" className="px-3 py-5 font-medium">
+                    Total Invoices
+                  </th>
+                  <th scope="col" className="px-3 py-5 font-medium">
+                    Total Pending
+                  </th>
+                  <th scope="col" className="px-4 py-5 font-medium">
+                    Total Paid
+                  </th>
+                </tr>
                 </thead>
 
                 <tbody className="divide-y divide-gray-200 text-gray-900">
-                  {customers.map((customer) => (
-                    <tr key={customer.id} className="group">
-                      <td className="whitespace-nowrap bg-white py-5 pl-4 pr-3 text-sm text-black group-first-of-type:rounded-md group-last-of-type:rounded-md sm:pl-6">
-                        <div className="flex items-center gap-3">
-                          <Image
-                            src={customer.image_url}
-                            className="rounded-full"
-                            alt={`${customer.name}'s profile picture`}
-                            width={28}
-                            height={28}
-                          />
-                          <p>{customer.name}</p>
-                        </div>
-                      </td>
-                      <td className="whitespace-nowrap bg-white px-4 py-5 text-sm">
-                        {customer.email}
-                      </td>
-                      <td className="whitespace-nowrap bg-white px-4 py-5 text-sm">
-                        {customer.total_invoices}
-                      </td>
-                      <td className="whitespace-nowrap bg-white px-4 py-5 text-sm">
-                        {customer.total_pending}
-                      </td>
-                      <td className="whitespace-nowrap bg-white px-4 py-5 text-sm group-first-of-type:rounded-md group-last-of-type:rounded-md">
-                        {customer.total_paid}
-                      </td>
-                    </tr>
-                  ))}
+                {customers.map((customer) => (
+                  <tr key={customer.id} className="group">
+                    <td
+                      className="whitespace-nowrap bg-white py-5 pl-4 pr-3 text-sm text-black group-first-of-type:rounded-md group-last-of-type:rounded-md sm:pl-6">
+                      <div className="flex items-center gap-3">
+                        <Image
+                          src={customer.image_url}
+                          className="rounded-full"
+                          alt={`${customer.name}'s profile picture`}
+                          width={28}
+                          height={28}
+                        />
+                        <p>{customer.name}</p>
+                      </div>
+                    </td>
+                    <td
+                      className="whitespace-nowrap bg-white px-4 py-5 text-sm">
+                      {customer.email}
+                    </td>
+                    <td
+                      className="whitespace-nowrap bg-white px-4 py-5 text-sm">
+                      {customer.total_invoices}
+                    </td>
+                    <td
+                      className="whitespace-nowrap bg-white px-4 py-5 text-sm">
+                      {customer.total_pending}
+                    </td>
+                    <td
+                      className="whitespace-nowrap bg-white px-4 py-5 text-sm group-first-of-type:rounded-md group-last-of-type:rounded-md">
+                      {customer.total_paid}
+                    </td>
+                  </tr>
+                ))}
                 </tbody>
               </table>
             </div>
