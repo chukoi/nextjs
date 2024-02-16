03d00d85b367fa7b51ec616b0b59873ca7f9d063
Dean Heffernan <dean.heffernan@nextpracticehealth.com>
2024-02-16 15:25:49 +1100

    removing some build errors

--- auth.ts
+++ auth.ts
@@ -19,6 +19,7 @@ async function getUser(email: string): Promise<User | undefined> {
 export const {auth, signIn, signOut} = NextAuth({
   ...authConfig,
   providers: [Credentials({
+    // @ts-ignore
     async authorize(credentials) {
       const parsedCredentials = z
         .object({email: z.string().email(), password: z.string().min(6)})
@@ -27,6 +28,7 @@ export const {auth, signIn, signOut} = NextAuth({
       if (parsedCredentials.success) {
         const { email, password } = parsedCredentials.data;
         const user = await getUser(email);
+        if (!user) return null;
         const passwordsMatch = await bcrypt.compare(password, user.password);
 
         if (passwordsMatch) return user;
